4710539b85a622adfee1892900944e82a568eab5
MrDallOca <tdalloca@gmail.com>
2019-11-27 14:27:58 -0300

    fix lucas merge

--- loco.c
+++ loco.c
@@ -1,16 +1,9 @@
 #include "api_robot.h"
 
 int main(){
-
-<<<<<<< HEAD
-	set_torque(10, 10);
-	
-	while(1) { }
-=======
 	set_torque(100, 100);
 
 	while (1) { }
->>>>>>> 76715314d51b38a75738062020bfeebec4dff4de
 
 	return 0;
 }
